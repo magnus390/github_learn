5837c8f338ec24d20aeb96d40f3a5c68f537f5c0
UMANG CHUDASMA <umang84531@gmail.com>
2022-09-08 13:06:59 -0400

    Second commit  = Foote.php and Lab1.php

---
+++ Footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Footer File</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <footer><h2>Welcome to the Footer Page</h2></footer>
+</body>
+
+</html>
---
+++ Lab1.php
@@ -0,0 +1,24 @@
+<?php
+/*
+// get the data from the request
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Name Test</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <h2>Lecture 1</h2>
+    <p>This is the Lab1 of the PHP subject</p>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
