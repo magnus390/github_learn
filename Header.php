f2a370861641879b4e9dae0a11f13b04cc3d9faa
UMANG CHUDASMA <umang84531@gmail.com>
2022-09-08 13:04:45 -0400

    First commit  = Header.php

---
+++ Header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Header File</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>Welcome to the Header Page</header>
+</body>
+
+</html>
